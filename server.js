89045b146acd4d662c144302e7524729c41d802b
Sohbis <Soham.biswas.19@gmail.com>
2018-04-09 04:03:27 +0530

    d1

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/Json-server/public'));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4000);
