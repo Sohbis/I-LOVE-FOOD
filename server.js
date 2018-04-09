2b1f36de00294605249745bb4a74b205b0661344
Sohbis <Soham.biswas.19@gmail.com>
2018-04-10 02:00:27 +0530

    dp02

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const path = require('./Json-server')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
