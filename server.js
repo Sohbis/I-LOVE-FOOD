2cecd3b88f3a4808f184f9cb8a5e9ea2a666e817
Sohbis <Soham.biswas.19@gmail.com>
2018-04-19 10:54:34 +0530

    test-deploy1

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./Json-server/db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('./Json-server/db.json');
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
+// server.use(middlewares);
+// server.use(router);
 
-server.listen(port);
+// server.listen(port);
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./Json-server/db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
